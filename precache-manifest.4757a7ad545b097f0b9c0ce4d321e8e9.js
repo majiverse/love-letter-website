164d33a58889360e65fc37d1bb3f1eaf8ef11b03
evlosK <khntrrs@gmail.com>
2024-02-15 03:50:09 +0800

    Updates

---
+++ precache-manifest.4757a7ad545b097f0b9c0ce4d321e8e9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722c2619b95fbbafc15a8651a47a999d",
+    "url": "/love-letter-website/index.html"
+  },
+  {
+    "revision": "bc2271ec91ca0f245063",
+    "url": "/love-letter-website/static/css/main.87133355.chunk.css"
+  },
+  {
+    "revision": "84ff3e155a728cc943cf",
+    "url": "/love-letter-website/static/js/2.9381843f.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/love-letter-website/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "bc2271ec91ca0f245063",
+    "url": "/love-letter-website/static/js/main.4e86bb0a.chunk.js"
+  },
+  {
+    "revision": "7f43aed9688ca358212c",
+    "url": "/love-letter-website/static/js/runtime~main.e1beb5d3.js"
+  },
+  {
+    "revision": "b7593b66e35a9d89cc82247418844cb2",
+    "url": "/love-letter-website/static/media/aa.b7593b66.jpg"
+  },
+  {
+    "revision": "b813da0aa88ee75ee0777776d4c05e8f",
+    "url": "/love-letter-website/static/media/bb.b813da0a.jpg"
+  },
+  {
+    "revision": "896533da00b22e5c149baaa9838dd936",
+    "url": "/love-letter-website/static/media/camera.896533da.svg"
+  },
+  {
+    "revision": "5e8d55331045d44e64edaa5730b996f3",
+    "url": "/love-letter-website/static/media/cc.5e8d5533.jpg"
+  },
+  {
+    "revision": "a088d5c31919c37596bd0dc5a29e15fe",
+    "url": "/love-letter-website/static/media/dd.a088d5c3.jpg"
+  },
+  {
+    "revision": "67ff917a89c76a4516957738e450351f",
+    "url": "/love-letter-website/static/media/ee.67ff917a.jpg"
+  },
+  {
+    "revision": "2f837f3fce9cb9188b51af619e0d1a23",
+    "url": "/love-letter-website/static/media/envelope.2f837f3f.svg"
+  },
+  {
+    "revision": "e4b6a8d71f7a24e9599bafafc2ad763f",
+    "url": "/love-letter-website/static/media/gift.e4b6a8d7.svg"
+  },
+  {
+    "revision": "4f3bc3a14fca9829fb03fcd5a20dcc6b",
+    "url": "/love-letter-website/static/media/left.4f3bc3a1.svg"
+  },
+  {
+    "revision": "344544411a8689b4a40184cd0a6fbcb8",
+    "url": "/love-letter-website/static/media/ribbon.34454441.svg"
+  },
+  {
+    "revision": "16bdc75b47198af7ffbde9333f53e2dd",
+    "url": "/love-letter-website/static/media/right.16bdc75b.svg"
+  },
+  {
+    "revision": "fe85cc1841e9b70abb6e32f969464400",
+    "url": "/love-letter-website/static/media/ticket.fe85cc18.svg"
+  }
+]);
